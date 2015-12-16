160f4effc2fcb2e07fecc98ed46418e77452a958
Kabir Mohammed <mhkabir98@gmail.com>
2015-12-16 23:01:05 +0530

    Initial commit

--- README.md
+++ README.md
@@ -1,1 +1,34 @@
-# Neural Network based Object Classifier for Artemis MAVs
+# Neural Network based Object Classifier
+
+### Install dependencies : 
+
+Requires ROS for running
+
+```bash
+sudo apt-get install --no-install-recommends libboost-all-dev libgflags-dev libgoogle-glog-dev liblmdb-dev libprotobuf-dev libleveldb-dev libsnappy-dev libhdf5-serial-dev protobuf-compiler libatlas-base-dev
+```
+### Build Caffe : 
+
+```bash
+roscd aerial_detection
+cd caffe
+cp Makefile.config.example Makefile.config
+```
+For CPU-only Caffe, uncomment CPU_ONLY := 1 in Makefile.config. 
+```bash
+nano Makefile.config
+```
+```bash
+make all
+make distribute
+# hack for build issues
+protoc src/caffe/proto/caffe.proto --cpp_out=.
+mkdir include/caffe/proto
+mv src/caffe/proto/caffe.pb.h include/caffe/proto
+```
+
+### Build package
+```bash
+cd ~/catkin_ws/
+catkin build aerial_detection # or catkin_make
+```
--- src/classifier.cpp
+++ src/classifier.cpp
@@ -1,83 +1,73 @@
 #include <ros/ros.h>
 #include <ros/package.h>
-#include <std_msgs/String.h>
+#include <aerial_detection/detections.h>
 #include <image_transport/image_transport.h>
 #include <cv_bridge/cv_bridge.h>
 #include "classifier.h"
 
-const std::string RECEIVE_IMG_TOPIC_NAME = "/cam2/image_rect";
-const std::string PUBLISH_RET_TOPIC_NAME = "/detections";
+Classifier *classifier;
 
-Classifier* classifier;
-std::string model_path;
-std::string weights_path;
-std::string mean_file;
-std::string label_file;
-std::string image_path;
+ros::Publisher pred_pub;
+void publishPred(const std::vector<Prediction> &predictions);
 
-ros::Publisher gPublisher;
+void imageCallback(const sensor_msgs::ImageConstPtr &msg)
+{
+	try {
+		cv_bridge::CvImagePtr cv_ptr = cv_bridge::toCvCopy(msg, "bgr8");
 
-void publishRet(const std::vector<Prediction>& predictions);
-
-void imageCallback(const sensor_msgs::ImageConstPtr& msg) {
-    try {
-        cv_bridge::CvImagePtr cv_ptr = cv_bridge::toCvCopy(msg, "bgr8");
-        //cv::imwrite("rgb.png", cv_ptr->image);
 		cv::Mat img = cv_ptr->image;
 		std::vector<Prediction> predictions = classifier->Classify(img);
-		publishRet(predictions);
-    } catch (cv_bridge::Exception& e) {
-        ROS_ERROR("Could not convert from '%s' to 'bgr8'.", msg->encoding.c_str());
-    }
+		publishPred(predictions);
+
+	} catch (cv_bridge::Exception &e) {
+		ROS_ERROR("Could not convert from '%s' to 'bgr8'.", msg->encoding.c_str());
+	}
 }
 
-// TODO: Define a msg or create a service
-// Try to receive : $rostopic echo /caffe_ret
-void publishRet(const std::vector<Prediction>& predictions)  {
-    std_msgs::String msg;
-    std::stringstream ss;
-    for (size_t i = 0; i < predictions.size(); ++i) {
-        Prediction p = predictions[i];
-        ss << "[" << p.second << " - " << p.first << "]" << std::endl;
-    }
-    msg.data = ss.str();
-    gPublisher.publish(msg);
+void publishPred(const std::vector<Prediction> &predictions)
+{
+	aerial_detection::detections msg;
+	
+	// Publish 5 top predictions
+	for (size_t i = 0; i < 5; ++i) {	
+		Prediction p = predictions[i];
+		msg.detection[i] = p.first;
+		msg.detection_id[i] = p.second;
+	}
+
+	pred_pub.publish(msg);
 }
 
-int main(int argc, char **argv) {
-    ros::init(argc, argv, "aerial_detector_test");
-    ros::NodeHandle nh;
-    image_transport::ImageTransport it(nh);
-    
-    // To receive an image from the topic, PUBLISH_RET_TOPIC_NAME
-    image_transport::Subscriber sub = it.subscribe(RECEIVE_IMG_TOPIC_NAME, 1, imageCallback);
-	gPublisher = nh.advertise<std_msgs::String>(PUBLISH_RET_TOPIC_NAME, 100);
-    
-    std::string DATA_DIR = "/home/artemis/data";
-    
-    model_path = DATA_DIR + "/deploy.prototxt";
-    weights_path = DATA_DIR + "/bvlc_reference_caffenet.caffemodel";
-    mean_file = DATA_DIR + "/imagenet_mean.binaryproto";
-    label_file = DATA_DIR + "/synset_words.txt";
-    image_path = DATA_DIR + "/cows.jpg";
+int main(int argc, char **argv)
+{
+	ros::init(argc, argv, "aerial_detection");
+	ros::NodeHandle nh;
+	image_transport::ImageTransport it(nh);
 
-    classifier = new Classifier(model_path, weights_path, mean_file, label_file);
+	std::string data_directory;
+	std::string camera_topic;
+	nh.param<std::string>("data_directory", data_directory, "/home/artemis/data");
+	nh.param<std::string>("camera_topic", camera_topic, "/stereo/cam0/image_rect");
 
-    // TODO : read direct camera stream from callback
-    cv::Mat img = cv::imread(image_path, -1);
-    std::vector<Prediction> predictions = classifier->Classify(img);
-    /* Print the top N predictions. */
-    
-    std::cout << "Test default image" << std::endl;
-    for (size_t i = 0; i < predictions.size(); ++i) {
-        Prediction p = predictions[i];
-        std::cout << std::fixed << std::setprecision(4) << p.second << " - \"" << p.first << "\"" << std::endl;
-    }
-    
-    publishRet(predictions);
+	image_transport::Subscriber image_sub = it.subscribe(camera_topic, 1, imageCallback);
+	pred_pub = nh.advertise<aerial_detection::detections>("detections", 10);
+	
+	ROS_INFO("Starting neural network classifier");
 
-    ros::spin();
-    delete classifier;
-    ros::shutdown();
-    return 0;
+	std::string model_path = data_directory + "/deploy.prototxt";
+	std::string weights_path = data_directory + "/bvlc_reference_caffenet.caffemodel";
+	std::string mean_file = data_directory + "/imagenet_mean.binaryproto";
+	std::string label_file = data_directory + "/synset_words.txt";
+	std::string image_path = data_directory + "/cows.jpg";
+
+	classifier = new Classifier(model_path, weights_path, mean_file, label_file);
+	
+	while(ros::ok())
+	{
+		ros::spin();
+	}
+	
+	delete classifier;
+	ros::shutdown();
+	return 0;
 }
